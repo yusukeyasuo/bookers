c2b15b3bf34f18df58898398daeb36db5ad20f9f
Yusuke Yasuo <yasuo.y@dely.jp>
2019-07-14 04:18:03 +0000

    feat: add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Book.count == 0
+  Book.create(title: 'リーダブルコード', body: 'より良いコードを書くためのシンプルで実践的なテクニック')
+  Book.create(title: 'トラブル知らずのシステム設計', body: 'システム設計の要点を図解で説明してくれます')
+end
